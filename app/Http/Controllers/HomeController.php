67fe2c836bb5dc3dc43eeeb3ae6f8f7c7bfe3d77
Rahul Katre <smart1boy2rahul3@gmail.com>
2022-10-17 17:15:40 +0530

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    function index()
+    {
+        return view("home");
+    }
+    function about()
+    {
+        return view("about");
+    }
+    function contact()
+    {
+        return view("contact");
+    }
+    function admin()
+    {
+        return view("admin");
+    }
+}
